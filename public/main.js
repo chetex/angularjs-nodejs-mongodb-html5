9e9dcb0584eab71b9930435fb3acdf48fb340714
chetex <chetex69@gmail.com>
2014-03-05 18:07:20 +0100

    initial commit

---
+++ public/main.js
@@ -0,0 +1,42 @@
+//public/main.js
+
+var angularTodo = angular.module('angularJS2', []);
+
+function mainController($scope, $http) {
+    $scope.formData = {};
+
+    // Carga todos los TODO
+    $http.get('/api/todos')
+        .success(function(data) {
+            $scope.todos = data;
+            console.log(data)
+        })
+        .error(function(data) {
+            console.log('Error: ' + data);
+        });
+
+    // Cuando se añade un nuevo TODO
+    $scope.createTodo = function(){
+        $http.post('/api/todos', $scope.formData)
+            .success(function(data) {
+                $scope.formData = {};
+                $scope.todos = data;
+                console.log(data);
+            })
+            .error(function(data) {
+                console.log('Error:' + data);
+            });
+    };
+
+    // Borra un TODO
+    $scope.deleteTodo = function(id) {
+        $http.delete('/api/todos/' + id)
+            .success(function(data) {
+                $scope.todos = data;
+                console.log(data);
+            })
+            .error(function(data) {
+                console.log('Error:' + data);
+            });
+    };
+}
